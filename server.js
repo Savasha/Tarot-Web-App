ba720c54caded9df580d04302230d9c7a8def7d4
Savasha <55656417+Savasha@users.noreply.github.com>
2023-12-06 01:49:45 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const TelegramBot = require('node-telegram-bot-api');
+
+const app = express();
+const bot = new TelegramBot('YOUR_BOT_TOKEN', {polling: true});
+
+app.use(express.json());
+
+app.post('/webappquery', (req, res) => {
+    const queryId = req.body.query_id;
+    const result = // Ваши данные для отправки обратно в бота
+
+    bot.answerWebAppQuery(queryId, result)
+        .then(() => res.sendStatus(200))
+        .catch(err => console.error(err));
+});
+
+app.listen(3000, () => console.log('Server is running on port 3000'));
